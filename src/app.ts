5ec016388f73e12e39d40e45fd242575182ae33e
bruhoffman <bruhoffman@gmail.com>
2025-01-21 16:53:14 -0300

    Aula 1 video 40:30

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: "Hello, World!" })
+});
+
+app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
+    res.send();
+});
+
+app.post('/partners', (req, res) => {
+    const { name, email, password, company_name } = req.body;
+    console.log(name, email, password, company_name)
+    res.send();
+});
+
+app.post('/customers', (req, res) => {
+    const { name, email, password, address, telefone } = req.body
+});
+
+app.post('/events', (req, res) => {
+    const { name, description, date, location } = req.body
+});
+
+app.get('/events', (req, res) => {
+
+});
+
+app.get('/events/:eventId', (req, res) => {
+    const { eventId } = req.params;
+    console.log(eventId);
+    res.send();
+});
+
+app.listen(3000, () => {
+    console.log('Running in http:://localhost:3000')
+});
+
